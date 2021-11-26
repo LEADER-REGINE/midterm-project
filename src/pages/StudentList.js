f50fa3f2367cb17fd3c255351ef9eec0f8a71e70
Marc Angelo Chiapco <marcchiapco34@gmail.com>
2021-11-26 16:20:22 +0800

    baseline

---
+++ src/pages/StudentList.js
@@ -0,0 +1,21 @@
+import '../App.css';
+import { useDispatch } from 'react-redux';
+import { getTheme } from '../redux/actions/uiAction';
+import { useEffect } from 'react';
+
+function StudentList() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getTheme());
+    }, [dispatch]);
+
+    return (
+        <Text>Hello</Text>
+
+
+    );
+}
+
+export default StudentList;
---
+++ src/router/index.js
@@ -0,0 +1,80 @@
+import '../App.css';
+
+import React from "react";
+import { ThemeProvider, createTheme } from '@mui/material';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import StudentList from '../pages/StudentList';
+import { useSelector } from 'react-redux';
+import { CssBaseline } from '@mui/material';
+
+
+export default function RouterComponent() {
+    const ui = useSelector((state) => state.ui);
+
+    /*  const darkTheme = createTheme({
+         palette: {
+             primary: {
+                 main: '#2D539E',
+                 dark: '#84A0C6',
+                 light: '#2D539E',
+             },
+             secondary: {
+                 main: '#80b2bd',
+             },
+             text: {
+                 primary: '#33374C',
+                 secondary: '#2D539E',
+                 disabled: '#ADB1C4',
+             },
+             error: {
+                 main: '#FC7374',
+             },
+             background: {
+                 paper: '#C6C8D1',
+                 default: '#161821',
+             },
+         },
+ 
+         typography: {
+             fontFamily: 'Inconsolata',
+         },
+     })
+ 
+     const lightTheme = createTheme({
+         palette: {
+             primary: {
+                 main: '#33374C',
+             },
+             secondary: {
+                 main: '#2D539E',
+             },
+             text: {
+                 primary: '#33374C',
+                 secondary: '#2D539E',
+                 disabled: '#ADB1C4',
+             },
+             error: {
+                 main: '#FC7374',
+             },
+             background: {
+                 paper: '#f9fbfd',
+                 default: '#E8E9EC',
+             },
+         },
+ 
+         typography: {
+             fontFamily: 'Inconsolata',
+         },
+     }) */
+    return (
+        <ThemeProvider>
+            <CssBaseline />
+            <Router>
+                <Switch>
+                    <Route exact component={StudentList} path="/" />
+                </Switch>
+            </Router>
+        </ThemeProvider>
+    );
+}
+

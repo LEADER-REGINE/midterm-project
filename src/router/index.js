14591813e10ce8ef9b0b315977c516b74b1795ec
Marc Angelo Chiapco <marcchiapco34@gmail.com>
2021-11-26 17:58:51 +0800

    appbar done
    login wip

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import '../App.css';
 
 import React from "react";
-import { ThemeProvider, createTheme } from '@mui/material';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import StudentList from '../pages/StudentList';
 import { useSelector } from 'react-redux';
@@ -10,63 +10,64 @@ import { useSelector } from 'react-redux';
 export default function RouterComponent() {
     const ui = useSelector((state) => state.ui);
 
-    /*  const darkTheme = createTheme({
-         palette: {
-             primary: {
-                 main: '#2D539E',
-                 dark: '#84A0C6',
-                 light: '#2D539E',
-             },
-             secondary: {
-                 main: '#80b2bd',
-             },
-             text: {
-                 primary: '#33374C',
-                 secondary: '#2D539E',
-                 disabled: '#ADB1C4',
-             },
-             error: {
-                 main: '#FC7374',
-             },
-             background: {
-                 paper: '#C6C8D1',
-                 default: '#161821',
-             },
-         },
- 
-         typography: {
-             fontFamily: 'Inconsolata',
-         },
-     })
- 
-     const lightTheme = createTheme({
-         palette: {
-             primary: {
-                 main: '#33374C',
-             },
-             secondary: {
-                 main: '#2D539E',
-             },
-             text: {
-                 primary: '#33374C',
-                 secondary: '#2D539E',
-                 disabled: '#ADB1C4',
-             },
-             error: {
-                 main: '#FC7374',
-             },
-             background: {
-                 paper: '#f9fbfd',
-                 default: '#E8E9EC',
-             },
-         },
- 
-         typography: {
-             fontFamily: 'Inconsolata',
-         },
-     }) */
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: '#1e1f20',
+                dark: '#84A0C6',
+                light: '#2D539E',
+            },
+            secondary: {
+                main: '#80b2bd',
+            },
+            text: {
+                primary: '#33374C',
+                secondary: '#2D539E',
+                disabled: '#ADB1C4',
+            },
+            error: {
+                main: '#FC7374',
+            },
+            background: {
+                paper: '#C6C8D1',
+                default: '#161821',
+            },
+        },
+
+        typography: {
+            fontFamily: 'Inconsolata',
+        },
+    })
+
+    const lightTheme = createTheme({
+        palette: {
+            primary: {
+                main: '#33374C',
+            },
+            secondary: {
+                main: '#2D539E',
+            },
+            text: {
+                primary: '#33374C',
+                secondary: '#2D539E',
+                disabled: '#ADB1C4',
+            },
+            error: {
+                main: '#FC7374',
+            },
+            background: {
+                paper: '#f9fbfd',
+                default: '#E8E9EC',
+            },
+        },
+
+        typography: {
+            fontFamily: 'Lato',
+        },
+    })
     return (
-        <ThemeProvider>
+        <ThemeProvider theme={ui.isDarkMode ? darkTheme : lightTheme}>
+            <CssBaseline />
             <Router>
                 <Switch >
                     <Route exact component={StudentList} path="/" />

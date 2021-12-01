f2164cad2e902fc55541135ae231a4e4533b7daf
Marc Angelo Chiapco <marcchiapco34@gmail.com>
2021-12-01 12:14:18 +0800

    Update StudentList.js

--- src/pages/StudentList.js
+++ src/pages/StudentList.js
@@ -1,17 +1,8 @@
 import '../App.css';
 import { useDispatch } from 'react-redux';
 import { getTheme } from '../redux/actions/uiAction';
-import React, { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
+import React, { useEffect } from 'react';
 import * as Mui from '@mui/material';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import { studentData } from './studentinfo';
-import Footer from "../components/Footer"
-
-import firebase from "../config/firebase";
-import { onSnapshot } from '@firebase/firestore';
 import TopStudents from '../components/TopStudents';
 import LoS from '../components/LoS';
 
@@ -49,43 +40,7 @@ const style = {
             md: "38px",
         },
     },
-    studentContainer: {
-        display: "flex",
-        flexDirection: "row",
-        marginLeft: {
-            xs: "115px",
-            sm: "200px",
-            md: "324px",
-        },
-        flexWrap: "wrap",
-        "@media only screen and (max-width : 720px)": {
-            flexDirection: "column",
-        }
-    },
-    studentPaper: {
-        height: {
-            xs: "70px",
-            sm: "81px",
-            md: "111px",
-        },
-        width: {
-            xs: "160px",
-            sm: "171px",
-            md: "201px",
-        },
-        marginRight: "30px",
-        backgroundColor: "#1E1F20",
-        display: "flex",
-        alignItems: "flex-start",
-        position: "static",
-        marginTop: "20px",
-        border: "1px solid #303336",
 
-        "@media only screen and (max-width : 720px)": {
-            marginTop: "10px",
-        }
-
-    },
 };
 
 

13ca089fe1b02b4515802b67d6fd1b18944462e1
Jym-ship-it <fjymcyruz@gmail.com>
2021-12-03 16:46:19 +0800

    redesign

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,205 +1,212 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import SearchIcon from '@mui/icons-material/Search';
-import LoginModal from './LoginModal';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import { Link, NavLink } from 'react-router-dom';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import SearchIcon from "@mui/icons-material/Search";
+import LoginModal from "./LoginModal";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import { Link, NavLink } from "react-router-dom";
 
-
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-    },
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
-        },
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
     },
+  },
 }));
 
 const style = {
-    appBar: {
-        backgroundColor: "#1E1F20",
-    }
+  appBar: {
+    backgroundColor: "#1E1F20",
+  },
 };
 
 const links = [
-    {
-        to: '/',
-        name: 'Student List'
-    },
-    {
-        to: '/evaluation',
-        name: 'Student Evaluation'
-    },
-    {
-        to: '/blog',
-        name: 'Blog'
-    },
-]
-
-
-
+  {
+    to: "/",
+    name: "Student List",
+  },
+  {
+    to: "/evaluation",
+    name: "Student Evaluation",
+  },
+  {
+    to: "/blog",
+    name: "Blog",
+  },
+];
 
 export default function Navbar() {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={style.appBar}>
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-                        >
-                            Student Review
-                        </Typography>
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={style.appBar}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+            >
+              Student Review
+            </Typography>
 
-                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="inherit"
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{
-                                    display: { xs: 'block', md: 'none' },
-                                }}
-                            >
-
-                                {links.map(link => (
-                                    <NavLink onClick={handleCloseNavMenu} to={link.to} style={{ textDecoration: 'none' }}>
-                                        <MenuItem key={link.to} onClick={handleCloseNavMenu}>
-                                            <Typography textAlign="center" sx={{ my: 2, color: 'black', display: 'block' }}>{link.name}</Typography>
-                                        </MenuItem>
-                                    </NavLink>
-                                )
-                                )}
-                            </Menu>
-                        </Box>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-                        >
-                            Student Review
-                        </Typography>
-                        <Box sx={{
-                            flexGrow: 1, display: { xs: 'none', md: 'flex' },
-                            alignContent: 'center', alignItems: 'center',
-                            justifyContent: 'center', justifyItems: 'center'
-                        }}>
-                            {links.map(link => (
-                                <NavLink 
-                                    style={
-                                        isActive => ({
-                                        color: isActive ? "#26CE8D" : "#D1D4C9",
-                                        borderTop: isActive ? '3px solid #26CE8D': '0px solid #26CE8D',
-                                        textDecoration: 'none',
-                                        margin: '20px',
-                                       
-                                        })} 
-                                  onClick={handleCloseNavMenu} 
-                                  to={link.to}  
-                                  exact={true}
-                                >
-                                    {link.name}
-                                    {/* <Button sx={{ my: 2, color: 'white', display: 'block' }} style={{ textDecoration: 'none' }}> {link.name} </Button> */}
-                                </NavLink>)
-                            )}
-                        </Box>
-                        <LoginModal />
-                        <Search>
-                            <SearchIconWrapper>
-                                <SearchIcon />
-                            </SearchIconWrapper>
-                            <StyledInputBase
-                                placeholder="Search…"
-                                inputProps={{ 'aria-label': 'search' }}
-                            />
-                        </Search>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-        </Box >
-    );
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {links.map((link) => (
+                  <NavLink
+                    onClick={handleCloseNavMenu}
+                    to={link.to}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <MenuItem key={link.to} onClick={handleCloseNavMenu}>
+                      <Typography
+                        textAlign="center"
+                        sx={{ my: 2, color: "black", display: "block" }}
+                      >
+                        {link.name}
+                      </Typography>
+                    </MenuItem>
+                  </NavLink>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+            >
+              Student Review
+            </Typography>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex" },
+                alignContent: "center",
+                alignItems: "center",
+                justifyContent: "center",
+                justifyItems: "center",
+              }}
+            >
+              {links.map((link) => (
+                <NavLink
+                  style={(isActive) => ({
+                    color: isActive ? "#26CE8D" : "#D1D4C9",
+                    borderTop: isActive
+                      ? "3px solid #26CE8D"
+                      : "0px solid #26CE8D",
+                    textDecoration: "none",
+                    margin: "20px",
+                  })}
+                  onClick={handleCloseNavMenu}
+                  to={link.to}
+                  exact={true}
+                >
+                  {link.name}
+                  {/* <Button sx={{ my: 2, color: 'white', display: 'block' }} style={{ textDecoration: 'none' }}> {link.name} </Button> */}
+                </NavLink>
+              ))}
+            </Box>
+            <LoginModal />
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
 }
--- src/pages/StudentList.js
+++ src/pages/StudentList.js
@@ -1,67 +1,65 @@
-import '../App.css';
-import { useDispatch } from 'react-redux';
-import { getTheme, getID } from '../redux/actions/uiAction';
-import React, { useEffect } from 'react';
-import * as Mui from '@mui/material';
-import TopStudents from '../components/TopStudents';
-import LoS from '../components/LoS';
+import "../App.css";
+import { useDispatch } from "react-redux";
+import { getTheme, getID } from "../redux/actions/uiAction";
+import React, { useEffect } from "react";
+import * as Mui from "@mui/material";
+import TopStudents from "../components/TopStudents";
+import LoS from "../components/LoS";
 import { Helmet } from "react-helmet";
 
 const style = {
-    root: {
-        width: "100%",
-        background: "#131414",
+  root: {
+    width: "100%",
+    background: "#131414",
+  },
+  topStudent: {
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: {
+      xs: "10px",
+      sm: "14px",
+      md: "18px",
     },
-    topStudent: {
-        fontStyle: "normal",
-        fontWeight: "bold",
-        fontSize: {
-            xs: "10px",
-            sm: "14px",
-            md: "18px",
-        },
-        background: "#131414",
-        display: "flex",
-        alignItems: "center",
-        textAlign: "center",
-        color: "white",
-        paddingTop : "38px",
-        paddingBottom : "0px"
-    },
-
+    background: "#131414",
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    color: "white",
+    paddingTop: "38px",
+    paddingBottom: "0px",
+  },
 };
 
-
 function StudentList() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getTheme());
-        dispatch(getID());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getTheme());
+    dispatch(getID());
+  }, [dispatch]);
 
-    return (
-
-        <Mui.Box sx={style.root}>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>Student List</title>
-                <meta name="description" content="View list of students and ratings" />
-                <meta name="keywords" content="Review, Ratings, Student Review, Student Ratings" />
-            </Helmet>
-            <Mui.Box color="background">
-                <Mui.Container>
-                    <Mui.Box component="label" sx={style.topStudent}>
-                        Top Students
-                    </Mui.Box>
-                </Mui.Container>
-                <TopStudents />
-                <LoS />
-            </Mui.Box>
-
-        </Mui.Box >
-
-    );
+  return (
+    <Mui.Box sx={style.root}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Student List</title>
+        <meta name="description" content="View list of students and ratings" />
+        <meta
+          name="keywords"
+          content="Review, Ratings, Student Review, Student Ratings"
+        />
+      </Helmet>
+      <Mui.Box color="background">
+        <Mui.Container>
+          <Mui.Box component="label" sx={style.topStudent}>
+            Top Students
+          </Mui.Box>
+        </Mui.Container>
+        <TopStudents />
+        <LoS />
+      </Mui.Box>
+    </Mui.Box>
+  );
 }
 
 export default StudentList;

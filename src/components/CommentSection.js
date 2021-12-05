5cdbfe0ec46726fd83b57d819211102db9e82c58
cristoferprago <cristoferprago0904@gmail.com>
2021-12-05 16:33:40 +0800

    comment

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 
 import firebase from "../config/firebase";
-import { Container, Paper, Typography, Button, IconButton, Avatar, Card, CardHeader, CardMedia, CardContent, CardActions, Collapse } from '@mui/material';
+import { Container, Paper, TextField, Typography, Button, IconButton, Avatar, Card, CardHeader, CardMedia, CardContent, CardActions, Collapse } from '@mui/material';
 import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 import CommentIcon from '@mui/icons-material/Comment';
 
@@ -22,6 +22,7 @@ import { shadows } from '@mui/system';
 
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import ReportGmailerrorredOutlinedIcon from '@mui/icons-material/ReportGmailerrorredOutlined';
+import ReplyIcon from '@mui/icons-material/Reply';
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -63,7 +64,7 @@ const style = {
             md: "30px",
         },
     },
-    
+
 }
 
 
@@ -98,6 +99,8 @@ export default function CommentSection() {
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
+
+
 
     return (
         <Box sx={{
@@ -144,24 +147,24 @@ export default function CommentSection() {
             {commentsList &&
                 commentsList.list.map((commentsList) => {
                     return (
-                        <Card sx={{ 
-                            width: "926px", 
-                            marginBottom: "24px", 
-                            bgcolor: "#1E1F20", 
+                        <Card sx={{
+                            width: "926px",
+                            marginBottom: "24px",
+                            bgcolor: "#1E1F20",
                             border: "1px solid #303336",
                             boxShadow: 4,
-                            }}>
+                        }}>
 
                             <CardHeader
                                 avatar={
-                                    <Avatar sx={{  }} aria-label="recipe">
-                                        
+                                    <Avatar sx={{}} aria-label="recipe">
+
                                     </Avatar>
                                 }
-                                
-                                
+
+
                                 title={commentsList.email}
-                                
+
                                 subheader="Time Posted"
                             />
 
@@ -172,29 +175,17 @@ export default function CommentSection() {
                                 icon={<StarRoundedIcon sx={style.rofilledStars} />}
                                 emptyIcon={<StarRoundedIcon sx={style.roemptyStars} />}
                                 value={commentsList.final_rating}
-                                sx={{marginLeft:"60px"}}
+                                sx={{ marginLeft: "60px" }}
                                 readOnly
                             />
 
                             <CardContent
-                            sx={{marginLeft:"52px"}}>
+                                sx={{ marginLeft: "52px" }}>
                                 <Typography variant="body2" color="#D1D4C9">
                                     {commentsList.review}
                                 </Typography>
                             </CardContent>
                             <CardActions >
-                                
-                                <ExpandMore
-                                    expand={expanded}
-                                    onClick={handleExpandClick}
-                                    aria-expanded={expanded}
-                                    aria-label="show more"
-                                >
-                                    <ChatBubbleOutlineIcon sx={{color:"#62666D", marginRight:"12px"}} />
-                                    <Typography variant="body2" color="#62666D">
-                                        Comment
-                                    </Typography>
-                                </ExpandMore>
 
                                 <ExpandMore
                                     expand={expanded}
@@ -202,16 +193,63 @@ export default function CommentSection() {
                                     aria-expanded={expanded}
                                     aria-label="show more"
                                 >
-                                    <ReportGmailerrorredOutlinedIcon sx={{color:"#62666D", marginRight:"12px"}} />
+                                    <ExpandMoreIcon sx={{ color: "#62666D" }} />
+                                    {/* <ChatBubbleOutlineIcon sx={{color:"#62666D", marginRight:"12px"}} />
+                                    <Typography variant="body2" color="#62666D">
+                                        Comment
+                                    </Typography> */}
+                                </ExpandMore>
+
+                                {/* <ExpandMore
+                                    expand={expanded}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    {/* <ReportGmailerrorredOutlinedIcon sx={{color:"#62666D", marginRight:"12px"}} />
                                     <Typography variant="body2" color="#62666D">
                                         Report
-                                    </Typography>
-                                </ExpandMore>
-                                
+                                    </Typography> */}
+                                {/* </ExpandMore> */}
+
                             </CardActions>
                             <Collapse in={expanded} timeout="auto" unmountOnExit>
                                 <CardContent>
-                                    
+                                    <Card sx={{ background: "#2C2F31", padding: "20px" }}>
+                                        <CardActions sx={{ color: "#62666D" }}>
+                                            <ReplyIcon />
+                                            <Typography sx={{ marginLeft: "12px" }}>Add your comment</Typography>
+                                        </CardActions>
+                                        <CardActions >
+                                            <TextField
+                                                type="text"
+
+                                                name="commentBody"
+                                                //onChange={userInput("commentBody")}
+                                                //value={payload.commentBody}
+                                                sx={{  width: "100%" }}
+                                            />
+                                            <Button sx={{ height: "44px", backgroundColor: "#26CE8D", width: "143px", color: "#fff", marginLeft: "16px" }}>
+                                                <Typography sx={{fontSize: "14px"}}>
+                                                    Submit
+                                                </Typography>
+                                            </Button>
+                                        </CardActions>
+                                    </Card>
+
+
+                                    <div
+                                        style={{
+                                            display: "flex",
+                                            flexDirection: "row",
+                                            justifyContent: "flex-end",
+                                            alignItems: "center",
+                                            marginTop: 20,
+                                        }}
+                                    >
+                                        {/* <Button onClick={() => handleComment()}>Comment</Button> */}
+
+                                    </div>
                                 </CardContent>
                             </Collapse>
 
